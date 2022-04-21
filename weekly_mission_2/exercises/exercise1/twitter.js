9667e3644e0cb6f4f186f6458c50c0121709cef1
Cecilia Muñoz <88991127+CeViMu@users.noreply.github.com>
2022-04-21 09:07:11 -0500

    Agregando ejemplo de objeto twitter

---
+++ weekly_mission_2/exercises/exercise1/twitter.js
@@ -0,0 +1,14 @@
+const twitter = {
+    user: {
+        user: "Cecilia Vianey Muñoz Chávez",
+        username: "@CeciliaMuC",
+        age: "23",
+        followers: 23,
+        following:11,
+        likes: 20,
+    },
+    trending_topic: ["#Feliz jueves","#TRAIDORES_A_LA_PATRIA","#Salario Rosa","#ZamtastiKorno"],
+    hashtag: ["RobertSmith","JuanPa Zurita","Marihuana"] 
+}
+
+console.log(twitter)
