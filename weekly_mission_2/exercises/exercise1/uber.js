637479f2c5d19f371ff364bcb5b92e401efcec67
Cecilia Muñoz <88991127+CeViMu@users.noreply.github.com>
2022-04-22 17:49:18 -0500

    Create uber.js

---
+++ weekly_mission_2/exercises/exercise1/uber.js
@@ -0,0 +1,10 @@
+const uber={
+    profile:{
+        Number_phone: 4428646672,
+        name: "Cecilia Muñoz",
+        email: "cecymunoz238@gmail.com"
+    },
+    travel: ["Centro Histórico-Plaza de los mariachis", "Jardin Zenea-Hospital general","Palacio municipal-Avenida Universidad"]
+}
+console.log(uber.travel)
+console.log(uber.profile.email)
