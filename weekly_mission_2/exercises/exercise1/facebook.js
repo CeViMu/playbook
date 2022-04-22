aa53362ee702589626be404a490708dd5b358c5d
Cecilia Muñoz <88991127+CeViMu@users.noreply.github.com>
2022-04-22 17:47:35 -0500

    Agregando ejemplo de objeto facebook

---
+++ weekly_mission_2/exercises/exercise1/facebook.js
@@ -0,0 +1,16 @@
+const facebook={
+    user: "Cecy Muñoz",
+    post: 3,
+    followers: 13,
+    biography:{
+        numberOfFriends: 167,
+        photos: 5,
+        Workplace: "Preparatoria Tierra Blanca",
+        University: "Universidad Autónoma de Querétaro",
+        City: "Tierra Blanca",
+        Celphone: "4428646672"
+    }
+}
+
+console.log(facebook);
+console.log("The user " + facebook.user + " have " + facebook.post + " posts")
